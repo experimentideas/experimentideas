c566908b35961d230b57f07eb07a7f0a002bf159
Gary Lupyan <glupyan@gmail.com>
2022-11-15 18:26:33 -0600

    Update documentation

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Writing Jekyll Posts",
+        "excerpt":"Test   Trying this out. Does it work?   What about a link  ","categories": [],
+        "tags": [],
+        "url": "/writing-jekyll-posts/",
+        "teaser": null
+      }]
